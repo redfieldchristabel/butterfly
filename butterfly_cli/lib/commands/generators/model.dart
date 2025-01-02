e7f272cf6d9e7acfb80b90b38076ff0472cd115a
cristabelredfield <christabelredfield@gmail.com>
2025-01-02 11:49:58 +0800

    docs: add

--- butterfly_cli/lib/commands/generators/model.dart
+++ butterfly_cli/lib/commands/generators/model.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:args/command_runner.dart';
 import 'package:butterfly_cli/extensions/command_helper.dart';
 
-class ModelGeneratorCommand extends Command with ButterflyLogger{
+class ModelGeneratorCommand extends Command with ButterflyLogger {
   @override
   String get description => 'TODO: Add description';
 
@@ -11,22 +11,13 @@ class ModelGeneratorCommand extends Command with ButterflyLogger{
   String get name => 'model';
 
   @override
-  List<String> get aliases => ['m', 'mld'];
+  List<String> get aliases => ['m', 'mdl'];
 
   @override
   FutureOr? run() {
     ensureRoot();
 
-    final version = pubspecService.version;
-    final File file = File('lib/version.dart');
-
-    detail('Generating version.dart');
-    // TODO: find a wat to use expression builder to create a dart code
-    final String context = "const String kVersion = '$version';";
-
-    detail('Generate file version.dart');
-    file.writeAsStringSync(context);
-    info('Generate file version.dart at ${Uri.file(file.absolute.path)}');
+    //   TODO: implement
+    throw UnimplementedError();
   }
-
 }
