8f8b98f82e415d73816dfa5e90e133aaf70c9fdf
cristabelredfield <christabelredfield@gmail.com>
2025-04-14 17:50:39 +0800

    feat(docs): add features page to documentation
    This commit adds a new "Features" page to the documentation, providing an overview of Butterfly's functionalities, including the CLI-based generator and library-based helpers. It also details CLI features like model generation, version file generation, and conventional commit message support.

---
+++ packages/auth_management_database_hive_ce/lib/base_user.dart
@@ -0,0 +1,4 @@
+import 'package:hive_ce/hive.dart';
+
+class BaseUser extends HiveObject {
+}
---
+++ packages/auth_management_database_hive_ce/test/auth_management_database_hive_ce_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:auth_management_database_hive_ce/auth_management_database_hive_ce.dart';
+
+void main() {
+  test('adds one to input values', () {
+  });
+}
