1945c48c31df6d8c7df45e8877fc5c9262de9f7a
cristabelredfield <christabelredfield@gmail.com>
2025-07-08 16:07:58 +0800

    feat: update RiverpodUserState to keep alive and adjust provider types

--- packages/auth_management_provider_riverpod/lib/src/user.g.dart
+++ packages/auth_management_provider_riverpod/lib/src/user.g.dart
@@ -6,12 +6,12 @@ part of 'user.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$riverpodUserStateHash() => r'19e5ed58db235cd12afcc88770c29f8d67c59bf8';
+String _$riverpodUserStateHash() => r'a635d76f357d5f7fd522f34e7b9a8b9a8e56614e';
 
 /// See also [RiverpodUserState].
 @ProviderFor(RiverpodUserState)
 final riverpodUserStateProvider =
-    AutoDisposeNotifierProvider<RiverpodUserState, RiverpodUser?>.internal(
+    NotifierProvider<RiverpodUserState, RiverpodUser?>.internal(
   RiverpodUserState.new,
   name: r'riverpodUserStateProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,6 @@ final riverpodUserStateProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$RiverpodUserState = AutoDisposeNotifier<RiverpodUser?>;
+typedef _$RiverpodUserState = Notifier<RiverpodUser?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
