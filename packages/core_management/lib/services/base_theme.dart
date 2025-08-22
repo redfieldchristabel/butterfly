b16c1fab0b4b925bab77b75fd89c4d67dc42b917
cristabelredfield <christabelredfield@gmail.com>
2025-08-22 10:14:10 +0800

    Correct theme data types
    The `AppBarTheme` and `InputDecorationTheme` getters in `BaseTheme` were returning the widget themes instead of the theme data. This commit corrects them to return `AppBarThemeData` and `InputDecorationThemeData` respectively.

--- packages/core_management/lib/services/base_theme.dart
+++ packages/core_management/lib/services/base_theme.dart
@@ -56,7 +56,7 @@ abstract class BaseThemeService {
     );
   }
 
-  AppBarTheme get appBarTheme => iTheme.appBarTheme;
+  AppBarThemeData get appBarTheme => iTheme.appBarTheme;
 
   CardThemeData get cardTheme => iTheme.cardTheme;
 
@@ -100,5 +100,5 @@ abstract class BaseThemeService {
 
   TextTheme get textTheme => iTheme.textTheme;
 
-  InputDecorationTheme get inputDecorationTheme => iTheme.inputDecorationTheme;
+  InputDecorationThemeData get inputDecorationTheme => iTheme.inputDecorationTheme;
 }
