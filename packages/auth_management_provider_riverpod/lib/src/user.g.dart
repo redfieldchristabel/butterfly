733cdda8676bd137cc2e4701d1bcb7bc53f0dac6
cristabelredfield <christabelredfield@gmail.com>
2026-06-18 15:28:13 +0800

    fix: upgrade dependency

--- packages/auth_management_provider_riverpod/lib/src/user.g.dart
+++ packages/auth_management_provider_riverpod/lib/src/user.g.dart
@@ -47,7 +47,7 @@ abstract class _$RiverpodUserState extends $Notifier<RiverpodUser?> {
   RiverpodUser? build();
   @$mustCallSuper
   @override
-  void runBuild() {
+  WhenComplete runBuild() {
     final ref = this.ref as $Ref<RiverpodUser?, RiverpodUser?>;
     final element =
         ref.element
@@ -57,6 +57,6 @@ abstract class _$RiverpodUserState extends $Notifier<RiverpodUser?> {
               Object?,
               Object?
             >;
-    element.handleCreate(ref, build);
+    return element.handleCreate(ref, build);
   }
 }
